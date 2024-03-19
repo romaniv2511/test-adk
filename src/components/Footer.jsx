cca4f39f0b5ed369a1233df2f1b30d90c01fd757
user <user@o-romaniv.local>
2024-03-19 17:47:57 +0200

    create app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Header() {
+    return (
+        <footer>
+            <div className="container">
+                <ul></ul>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="container">
+                <nav>
+                    <Link href='/'>Online casino paypal</Link>
+                    <Link href='/'>Paypal casino mobile</Link>
+                    <Link href='/'>Best paypal casino slots</Link>
+                    <Link href='/'>PayPal poker</Link>
+                    <Link href='/'>Paypal casino deposit</Link>
+                    <Link href='/'>Paypal casino withdraw</Link>
+                </nav>
+            </div>
+        </header>
+    )
+}
